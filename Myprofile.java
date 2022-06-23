4cb0e67158e3f4a7707c214213c9426df8aa69af
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-23 06:50:53 +0530

    Add files via upload

---
+++ Myprofile.java
@@ -0,0 +1,8 @@
+ class Myprofile
+{
+    public static void main(String args[]) {
+        System.out.println("Name:SUMIT DUTTA");
+        System.out.println("Course: Java web development");
+        System.out.println("Stream:CSE");
+      }
+}
