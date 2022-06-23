8710100d185077d1bcf7681fad33adb615f73fa1
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-23 06:51:42 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,19 @@
+
+class Calculation{
+                  public static void main(String[]args){
+                  
+
+                    int x=20;
+                    int y=45;
+                    int add=x+y;
+                    int sub=x-y;
+                    int multi=x*y;
+                    int div=x/y;
+
+
+                  System.out.println(add);
+                  System.out.println(sub);
+                  System.out.println(multi);
+                  System.out.println(div);
+}
+}
