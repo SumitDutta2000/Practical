c561d525c6305589feada9a13cb9d10e56886f1b
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-27 20:09:49 +0530

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,16 @@
+
+public class Dowhileloop {
+	public static void main(String[] args) {
+		
+		int i = 0;
+		System.out.println("First 10 even mumbers \n");
+		do {
+			System.out.println(i);
+			i = i + 2;
+		}while(i<=10);
+		
+		
+		}
+		
+	}
+
